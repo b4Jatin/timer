d4839602fc0992a8a75650628c5b3b91c75948e3
Jatin D <b4jatin@gmail.com>
2020-04-07 12:47:59 +1000

    files added

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('check check');
+let start = document.querySelector('#start');
+let stop = document.querySelector('#stop');
+let reset = document.querySelector('#reset');
+let display = document.querySelector('#display');
+
+
+
+start.addEventListener('click',()=>{console.log('blaa')});
+
+stop.addEventListener('click',()=>{console.log('blaa')});
+
+reset.addEventListener('click',()=>{console.log('blaa')});
